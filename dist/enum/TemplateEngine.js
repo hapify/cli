f59a40a4545e6aca027d183d9c9a9420380d8d3c
Edouard Demotes <e.demotes@gmail.com>
2019-03-06 02:36:34 -0500

    build

--- dist/enum/TemplateEngine.js
+++ dist/enum/TemplateEngine.js
@@ -1,3 +1,3 @@
-/*! hapify-cli 2019-03-03 */
+/*! hapify-cli 2019-03-06 */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});class TemplateEngine{}TemplateEngine.Hpf="hpf",TemplateEngine.JavaScript="js",exports.TemplateEngine=TemplateEngine;
--- dist/interface/schema/Template.js
+++ dist/interface/schema/Template.js
@@ -1,3 +1,3 @@
-/*! hapify-cli 2019-03-03 */
+/*! hapify-cli 2019-03-06 */
 
 "use strict";var __importStar=this&&this.__importStar||function(e){if(e&&e.__esModule)return e;var i={};if(null!=e)for(var t in e)Object.hasOwnProperty.call(e,t)&&(i[t]=e[t]);return i.default=e,i};Object.defineProperty(exports,"__esModule",{value:!0});const Joi=__importStar(require("joi")),enum_1=require("../../enum"),engines=[enum_1.TemplateEngine.Hpf,enum_1.TemplateEngine.JavaScript],inputs=[enum_1.TemplateInput.One,enum_1.TemplateInput.All];exports.TemplateSchema=Joi.object({path:Joi.string().required(),engine:Joi.string().valid(engines).required(),input:Joi.string().valid(inputs).required(),content:Joi.string().required().allow("")}),exports.ConfigTemplateSchema=Joi.object({path:Joi.string().required(),engine:Joi.string().valid(engines).required(),input:Joi.string().valid(inputs).required()});
