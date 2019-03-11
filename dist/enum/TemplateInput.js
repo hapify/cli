1a18f9ec7a09c11039a387b6bea374695edc77c7
Edouard Demotes <e.demotes@gmail.com>
2019-03-11 00:59:34 -0400

    Rebuild

--- dist/enum/TemplateInput.js
+++ dist/enum/TemplateInput.js
@@ -1,3 +1,3 @@
-/*! hapify-cli 2019-03-07 */
+/*! hapify-cli 2019-03-11 */
 
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});class TemplateInput{}TemplateInput.One="one",TemplateInput.All="all",exports.TemplateInput=TemplateInput;
