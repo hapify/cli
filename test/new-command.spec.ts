1f5fe72a3797793366a5f453a13fe35c5c679837
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-10-22 20:53:59 -0400

    test(new): improve new command's tests

--- test/new-command.spec.ts
+++ test/new-command.spec.ts
@@ -4,7 +4,7 @@ import 'mocha';
 import { CLI, Sandbox } from './helpers';
 
 describe('new command', () => {
-	it('success', async () => {
+	it('success by slug', async () => {
 		const sandbox = new Sandbox();
 		sandbox.clear();
 
@@ -16,7 +16,19 @@ describe('new command', () => {
 
 		expect(sandbox.fileExists(['hapify.json'])).to.be.true();
 	});
-	it('success with two', async () => {
+	it('success by id', async () => {
+		const sandbox = new Sandbox();
+		sandbox.clear();
+
+		const response = await CLI('new', ['--dir', sandbox.getPath(), '--boilerplate-id', '5c77197a98ebdb001075f3a5', '--no-presets']);
+
+		expect(response.stderr).to.be.empty();
+		expect(response.code).to.equal(0);
+		expect(response.stdout).to.contains('Created 1 new dynamic boilerplate');
+
+		expect(sandbox.fileExists(['hapify.json'])).to.be.true();
+	});
+	it('success by url (x2)', async () => {
 		const sandbox = new Sandbox();
 		sandbox.clear();
 
@@ -34,7 +46,18 @@ describe('new command', () => {
 		expect(response.code).to.equal(0);
 		expect(response.stdout).to.contains('Created 2 new dynamic boilerplates');
 
-		expect(sandbox.dirExists(['hapijs_tractr'])).to.be.true();
-		expect(sandbox.dirExists(['ngx_dashboard_tractr'])).to.be.true();
+		expect(sandbox.dirExists(['boilerplate-hapijs'])).to.be.true();
+		expect(sandbox.dirExists(['boilerplate-ngx-components'])).to.be.true();
+	});
+	it('busy folder', async () => {
+		const sandbox = new Sandbox();
+		sandbox.clear();
+		sandbox.touch('hapify.json', JSON.stringify({}));
+
+		const response = await CLI('new', ['--dir', sandbox.getPath(), '--boilerplate', 'hapijs_tractr', '--no-presets']);
+
+		expect(response.code).to.equal(1);
+		expect(response.stdout).to.be.empty();
+		expect(response.stderr).to.contains('folder is not empty');
 	});
 });
