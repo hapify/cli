cc0fe32e5e8ae0eb38edc88de290a239ae33e710
Edouard Demotes-Mainard <e.demotes@gmail.com>
2021-01-19 09:41:58 -0500

    fix: adapt field schema

--- src/interface/schema/Field.ts
+++ src/interface/schema/Field.ts
@@ -1,11 +1,29 @@
 import Joi from 'joi';
 
+const FieldTypes = ['boolean', 'number', 'string', 'enum', 'datetime', 'entity', 'object', 'file'];
+const FieldSubTypes = [
+	'integer',
+	'float',
+	'latitude',
+	'longitude',
+	'email',
+	'password',
+	'url',
+	'text',
+	'rich',
+	'date',
+	'time',
+	'image',
+	'video',
+	'audio',
+	'document',
+];
 export const FieldSchema = Joi.object({
 	name: Joi.string().required(),
 	notes: Joi.string().allow(null),
-	type: Joi.string().required(),
-	subtype: Joi.string().required().allow(null),
-	value: Joi.string().required().allow(null),
+	type: Joi.string().valid(FieldTypes).required(),
+	subtype: Joi.string().valid(FieldSubTypes).allow(null).required(),
+	value: Joi.alternatives().try(Joi.string(), Joi.array().items(Joi.string())).required().allow(null),
 	primary: Joi.boolean().required(),
 	unique: Joi.boolean().required(),
 	label: Joi.boolean().required(),
